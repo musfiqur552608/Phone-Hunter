2bb62201db052673c0dd9360630c5af960231f64
musfiqur552608 <mushfiqur15-10872@diu.edu.bd>
2022-08-30 22:30:15 +0600

    spinner added

--- js/app.js
+++ js/app.js
@@ -32,13 +32,25 @@ const displayPhones = phones =>{
         `;
 
         phonesContainer.appendChild(phoneDiv);
-    })
+    });
+    toggleSpinner(false);
 }
 
 document.getElementById('btn-search').addEventListener('click', function(){
+    toggleSpinner(true);
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     loadPhones(searchText);
 })
+
+const toggleSpinner = isLoading =>{
+    const loaderSection = document.getElementById('loader')
+    if(isLoading){
+        loaderSection.classList.remove('d-none');
+    }
+    else{
+        loaderSection.classList.add('d-none');
+    }
+}
 
 // loadPhones();
